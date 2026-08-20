Til7701/Puzzled

commit 25dd5eda2432bfab574abdf35dc0bede65afb74d

Refactoring (Models) (#159)
Refactors the main application.

Previously, there were presenters and views created using manual dependency injection. This apparently not in the spirit of glib. Also, accessing the state of puzzles and the application was a total mess and had a lot of workarounds.

Now, there are dedicated models for puzzles and collections, which centralize the access to static and dynamic metadata. They also cache some things for faster access. There are no presenters anymore. The views are handling their own layout and connect to signals from the models to update.

puzzled/src/model/store/community.rs
@@ -1,0 +1,63 @@
+use adw::glib;
+use log::{error, info};
+use std::path::PathBuf;
+
+pub fn save_community_collection(collection_id: &str, json_str: &str) {
+    let puzzles_dir = get_xdg_data_dir();
+    let file_path = puzzles_dir.join(format!("{}.json", collection_id));
+    if let Err(e) = std::fs::write(&file_path, json_str) {
+        error!("Failed to save community collection to file: {}", e);
+    }
+}
+
+pub fn load_community_collections() -> Vec<String> {
+    let puzzles_dir = get_xdg_data_dir();
+    let mut collections = Vec::new();
+
+    if let Ok(entries) = std::fs::read_dir(&puzzles_dir) {
+        for entry in entries.flatten() {
+            if let Ok(file_type) = entry.file_type()
+                && file_type.is_file()
+                && let Some(ext) = entry.path().extension()
+                && ext == "json"
+            {
+                if let Ok(json_str) = std::fs::read_to_string(entry.path()) {
+                    collections.push(json_str);
+                } else {
+                    error!(
+                        "Failed to read community collection file: {:?}",
+                        entry.path()
+                    );
+                }
+            }
+        }
+    } else {
+        error!("Failed to read puzzles directory: {:?}", puzzles_dir);
+    }
+
+    collections
+}
+
+pub fn delete_community_collection(collection_id: &str) {
+    let puzzles_dir = get_xdg_data_dir();
+    let file_path = puzzles_dir.join(format!("{}.json", collection_id));
+    if !file_path.exists() {
+        info!(
+            "Community collection file does not exist, nothing to delete: {:?}",
+            file_path
+        );
+        return;
+    }
+    if let Err(e) = std::fs::remove_file(&file_path) {
+        error!("Failed to delete community collection file: {}", e);
+    }
+}
+
+fn get_xdg_data_dir() -> PathBuf {
+    let xdg_data_dir = glib::user_data_dir();
+    let puzzles_dir = xdg_data_dir.join("../../..").join("community_puzzles");
+    if let Err(e) = std::fs::create_dir_all(&puzzles_dir) {
+        error!("Failed to create puzzles directory: {}", e);
+    }
+    puzzles_dir
+}

puzzled/src/puzzles/community.rs
@@ -1,63 +1,0 @@
-use adw::glib;
-use log::{error, info};
-use std::path::PathBuf;
-
-pub fn save_community_collection(collection_id: &str, json_str: &str) {
-    let puzzles_dir = get_xdg_data_dir();
-    let file_path = puzzles_dir.join(format!("{}.json", collection_id));
-    if let Err(e) = std::fs::write(&file_path, json_str) {
-        error!("Failed to save community collection to file: {}", e);
-    }
-}
-
-pub fn load_community_collections() -> Vec<String> {
-    let puzzles_dir = get_xdg_data_dir();
-    let mut collections = Vec::new();
-
-    if let Ok(entries) = std::fs::read_dir(&puzzles_dir) {
-        for entry in entries.flatten() {
-            if let Ok(file_type) = entry.file_type()
-                && file_type.is_file()
-                && let Some(ext) = entry.path().extension()
-                && ext == "json"
-            {
-                if let Ok(json_str) = std::fs::read_to_string(entry.path()) {
-                    collections.push(json_str);
-                } else {
-                    error!(
-                        "Failed to read community collection file: {:?}",
-                        entry.path()
-                    );
-                }
-            }
-        }
-    } else {
-        error!("Failed to read puzzles directory: {:?}", puzzles_dir);
-    }
-
-    collections
-}
-
-pub fn delete_community_collection(collection_id: &str) {
-    let puzzles_dir = get_xdg_data_dir();
-    let file_path = puzzles_dir.join(format!("{}.json", collection_id));
-    if !file_path.exists() {
-        info!(
-            "Community collection file does not exist, nothing to delete: {:?}",
-            file_path
-        );
-        return;
-    }
-    if let Err(e) = std::fs::remove_file(&file_path) {
-        error!("Failed to delete community collection file: {}", e);
-    }
-}
-
-fn get_xdg_data_dir() -> PathBuf {
-    let xdg_data_dir = glib::user_data_dir();
-    let puzzles_dir = xdg_data_dir.join("puzzled").join("community_puzzles");
-    if let Err(e) = std::fs::create_dir_all(&puzzles_dir) {
-        error!("Failed to create puzzles directory: {}", e);
-    }
-    puzzles_dir
-}

commit 99374255f642aa4f90755b141fd253bfc4f548aa

Fix saving of community puzzles
This broke in 25dd5eda2432bfab574abdf35dc0bede65afb74d due to refactoring shenanigans

puzzled/src/model/store/community.rs
@@ -55,7 +55,7 @@ pub fn delete_community_collection(collection_id: &str) {
 
 fn get_xdg_data_dir() -> PathBuf {
     let xdg_data_dir = glib::user_data_dir();
-    let puzzles_dir = xdg_data_dir.join("../../..").join("community_puzzles");
+    let puzzles_dir = xdg_data_dir.join("puzzled").join("community_puzzles");
     if let Err(e) = std::fs::create_dir_all(&puzzles_dir) {
         error!("Failed to create puzzles directory: {}", e);
     }