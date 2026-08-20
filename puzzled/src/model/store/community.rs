784e3f25def9471b17e6ec696e181fd549989703
Tilman Holube <tilman@holube.de>
2026-08-20 19:44:30 +0200

    Add tests

--- puzzled/src/model/store/community.rs
+++ puzzled/src/model/store/community.rs
@@ -3,7 +3,7 @@ use log::{error, info};
 use std::path::PathBuf;
 
 pub fn save_community_collection(collection_id: &str, json_str: &str) {
-    let puzzles_dir = get_xdg_data_dir();
+    let puzzles_dir = get_xdg_puzzles_data_dir();
     let file_path = puzzles_dir.join(format!("{}.json", collection_id));
     if let Err(e) = std::fs::write(&file_path, json_str) {
         error!("Failed to save community collection to file: {}", e);
@@ -11,7 +11,7 @@ pub fn save_community_collection(collection_id: &str, json_str: &str) {
 }
 
 pub fn load_community_collections() -> Vec<String> {
-    let puzzles_dir = get_xdg_data_dir();
+    let puzzles_dir = get_xdg_puzzles_data_dir();
     let mut collections = Vec::new();
 
     if let Ok(entries) = std::fs::read_dir(&puzzles_dir) {
@@ -39,7 +39,7 @@ pub fn load_community_collections() -> Vec<String> {
 }
 
 pub fn delete_community_collection(collection_id: &str) {
-    let puzzles_dir = get_xdg_data_dir();
+    let puzzles_dir = get_xdg_puzzles_data_dir();
     let file_path = puzzles_dir.join(format!("{}.json", collection_id));
     if !file_path.exists() {
         info!(
@@ -53,11 +53,47 @@ pub fn delete_community_collection(collection_id: &str) {
     }
 }
 
-fn get_xdg_data_dir() -> PathBuf {
+fn get_xdg_puzzles_data_dir() -> PathBuf {
     let xdg_data_dir = glib::user_data_dir();
     let puzzles_dir = xdg_data_dir.join("puzzled").join("community_puzzles");
     if let Err(e) = std::fs::create_dir_all(&puzzles_dir) {
         error!("Failed to create puzzles directory: {}", e);
     }
     puzzles_dir
+}
+
+#[cfg(test)]
+mod test {
+    use crate::model::store::community::{delete_community_collection, get_xdg_puzzles_data_dir, load_community_collections, save_community_collection};
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_save_delete_community_collection() {
+        save_community_collection("test", r#"{"test": true}"#);
+
+        let puzzles_dir = get_xdg_puzzles_data_dir();
+        println!("{:?}", puzzles_dir);
+
+        let file_path = puzzles_dir.join("test.json");
+        assert!(file_path.exists());
+
+        let all_collections_json = load_community_collections();
+        assert!(all_collections_json.contains(&r#"{"test": true}"#.to_string()));
+
+        delete_community_collection("test");
+
+        let all_collections_json = load_community_collections();
+        assert!(!all_collections_json.contains(&r#"{"test": true}"#.to_string()));
+    }
+
+    #[test]
+    fn test_get_xdg_puzzles_data_dir() {
+        let buf = get_xdg_puzzles_data_dir();
+
+        // Constructing strings this way to avoid search and replace refactoring errors. See #222
+        let puzzled = concat!("puz", "zled");
+        let community_puzzles = concat!("commun", "ity_puz", "zles");
+
+        assert!(buf.ends_with(PathBuf::from(puzzled).join(community_puzzles)));
+    }
 }
